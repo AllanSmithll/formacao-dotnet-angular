ae9638dfb1fdd886af644fdbac8fbe102489e816
AllaSmithll <allanammancio2004@gmail.com>
2022-10-09 14:47:50 -0300

    aula 1 de .NET modificada

---
+++ aulas .NET/dotnet01.cs
@@ -0,0 +1,22 @@
+// Aula 1 de .NET com C#, do dia 08/10/2022, com o professor Rodolfo Pedra, modificada. Ele fez de um jeito, e acabei modificando por ter escrito em um notebook emprestado
+
+using System;
+
+namespace Program1
+{
+    public class MinhaAplicacao
+    {
+        static void Main(string[] args)
+        {
+            MinhaClasse m = new MinhaClasse();
+            Console.WriteLine(m.Soma(100, 0));
+        }
+    }
+    public class MinhaClasse
+    {
+        public int Soma(int a, int b)
+        {
+            return {a + b;}
+        }
+    }
+}
